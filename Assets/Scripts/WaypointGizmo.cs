53a961218a35d013d4130c5157b7bfb4949e14a8
AmirKovalenko <amirkovalenko00@gmail.com>
2024-01-29 15:49:59 +0200

    Initial commit

---
+++ Assets/Scripts/WaypointGizmo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointGizmo : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, radius: 0.2f);
+    }
+}
